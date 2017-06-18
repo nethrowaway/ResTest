570b6dc58376a060d10046cbdb2d2b9ab764691e
nethrowaway <nickedwards69@googlemail.com>
2017-06-18 19:58:03 +0100

    add metadata helper test for title, description and keywords

--- tests/BDATest/Helpers/MetadataTest.php
+++ tests/BDATest/Helpers/MetadataTest.php
@@ -2,12 +2,44 @@
 
 use PHPUnit\Framework\TestCase;
 
-use BDATest\Helpers\Metadata;
+use ResTest\Helpers\Metadata;
 
 /**
  * @covers Metadata
  */
 class MetadataTest extends TestCase
 {
-    
+    protected function setUp()
+    {
+        // set error level to suppress warnings about non valid tags
+        $internalErrors = libxml_use_internal_errors(true);
+    }
+
+    public function providerGetMetadataFromUrl()
+    {
+        return [
+            'testSite' => [
+                'input' => [
+                    'url' => 'http://www.nick-edwards.co.uk/'
+                ],
+                'output' => [
+                  'title' => 'Nick Edwards | Web Developer',
+                  'description' => 'The Web Design and Development portfolio of Nick Edwards. A web developer, mainly working with PHP.',
+                  'keywords' => 'Nicholas Edwards, Nick Edwards, NDE Designs, ndedesigns, tsdesigns, twistedspikes, website, web design, web development, PHP, javascript, html, css, server side'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerGetMetadataFromUrl
+     **/
+    public function testGetMetadataFromUrl($input, $output)
+    {
+    	$metadata = new Metadata();
+        $this->assertEquals(
+            $output,
+            $metadata->getMetadataFromUrl($input['url'])
+        );
+    }
 }
